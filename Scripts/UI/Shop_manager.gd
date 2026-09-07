110e4622ed506d765e1d2ee057db648da7923874
Cafle <gardebun2@gmail.com>
2026-09-07 14:58:00 -0600

    shopmanager functionality proven

--- Scripts/UI/Shop_manager.gd
+++ Scripts/UI/Shop_manager.gd
@@ -3,20 +3,33 @@ extends Control
 @export var potions: int
 @export var columns: int
 
-@onready var potion = $VBoxContainer/CenterContainer/GridContainer/Potion
-@onready var grid = $VBoxContainer/CenterContainer/GridContainer
+@onready var potion = $CenterContainer/GridContainer/Potion
+@onready var grid = $CenterContainer/GridContainer
 @onready var Back = $back
+
+@export var Testdata : potionData
+@export var Fire : potionData
+@export var water : potionData
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
 	grid.columns = columns
 	var x = 0
-	for i in potions:
+	for i in potions - 1:
 		print(x)
 		x+=1
 		grid.add_child(potion.duplicate())
-	print
+	var potionslots = grid.get_children()
+	
+	potionslots[2].get_child(0).data = Fire
+	
+	potionslots[5].get_child(0).data = water
+	
+	for i in potionslots:
+		print(i.get_child(0).data.name)
+		
 	Back.button_up.connect(_back)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
