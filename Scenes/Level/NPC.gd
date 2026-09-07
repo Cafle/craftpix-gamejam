080ada6626cb3d3be5ce7a73c6d31938e492027c
tdizzzzzzle-bit <tdizzzzzzle@gmail.com>
2026-09-07 11:36:32 -0600

    NPC part 2

--- Scenes/Level/NPC.gd
+++ Scenes/Level/NPC.gd
@@ -1,20 +1,27 @@
 extends AnimatedSprite2D
 
-@export var col1: Color
-@export var col2: Color
-@export var col3: Color
-@export var col4: Color
-@export var col5: Color
-@export var col6: Color
-@export var col7: Color
+@export var SkinTone: Color
+@export var ShirtTone: Color
+@export var Ruffle1Tone: Color
+@export var Ruffle2Tone: Color
+@export var LegTone: Color
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	
+	#SUPER IMPORTANT TO VARY THE NPC SHADERS
+	material = material.duplicate()
+	material.set_shader_parameter("skin", SkinTone)
+	material.set_shader_parameter("dark_skin", SkinTone.darkened(0.1))
+	material.set_shader_parameter("shirt", ShirtTone)
+	material.set_shader_parameter("shirt_light", ShirtTone.darkened(-0.1))
+	material.set_shader_parameter("shirt_dark", ShirtTone.darkened(0.1))
+	material.set_shader_parameter("leg", SkinTone.darkened(0.1))
+	material.set_shader_parameter("leg_dark", SkinTone.darkened(0.2))
+	material.set_shader_parameter("ruffle1", Ruffle1Tone)
+	material.set_shader_parameter("ruffle2", Ruffle2Tone)
+	material.set_shader_parameter("ruffle_dark", Ruffle1Tone.darkened(0.1))
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	material.set_shader_parameter("key_color", col1)
-	material.set_shader_parameter("skin_color", Color(0.879, 0.715, 0.594, 1.0))
-	material.set_shader_parameter("key_color", col2)
-	material.set_shader_parameter("skin_color", Color(0.879, 0.715, 0.594, 1.0))
+	pass
+	
