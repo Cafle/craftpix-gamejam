39488eaab397f6849b2d0defff89a5aa0ee3297f
Cafle <gardebun2@gmail.com>
2026-09-13 01:35:52 -0600

    started dart

--- Scripts/Sprites/dart_shooter.gd
+++ Scripts/Sprites/dart_shooter.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var dart : PackedScene
-@export var signal_objects : Array [Node2D]
+@export var signal_objects : Array [Area2D]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,7 +12,10 @@ func _ready() -> void:
 func _shoot() -> void:
 	var newDart = dart.instantiate()
 	newDart.position = position
+	add_child(newDart)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("up"):
+		_shoot()
 	pass
---
+++ Scripts/dart.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export var dart : PackedScene
+@export var triggers : Array[PackedScene]
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("hello!")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	pass
