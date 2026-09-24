899560d345b2abe4930819b921735828e68ced36
tdizzzzzzle-bit <tdizzzzzzle@gmail.com>
2026-09-23 21:51:38 -0600

    made bricks sexy to work with

--- Scripts/Sprites/spawner.gd
+++ Scripts/Sprites/spawner.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+
 @export var spawn_me : PackedScene
 @export var or_duplicate : Node2D
 @export var time_between : int = 10
 @export var max : int = 20
+
+@export_category("Screenlock")
+@export var screen_lock = false
+@export var bind = Node2D
+@export var offset_x : float
+@export var offset_y : float
 
 var spawning = false
 
@@ -18,6 +25,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if screen_lock:
+		if bind:
+			position = bind.position
+			position.x += offset_x
+			position.y += offset_y
 	if or_duplicate:
 		print(or_duplicate)
 		if spawning and count <= 0 and scount <= max:
